2ad849679517a033e828cd0f2576225726ea52d1
mariaevrangel <102247534+mariaevrangel@users.noreply.github.com>
2023-06-26 21:36:44 -0300

    atualizada

--- src/Classes/ProdutosComuns.java
+++ src/Classes/ProdutosComuns.java
@@ -2,17 +2,17 @@ package Classes;
 
 public class ProdutosComuns {
 
-    public static final Produto banana = new Produto(1.5, 3, "Banana");
+    private static final Produto banana = new Produto(1.5, 3, "Banana");
 
-    public static final Produto maca = new Produto(1.5, 2, "Maçã");
+    private static final Produto maca = new Produto(1.5, 2, "Maçã");
 
-    public static final Produto refrigerante = new Produto(4, 3, "Refrigerante");
+    private static final Produto refrigerante = new Produto(4, 3, "Refrigerante");
 
-    public static final Produto agua = new Produto(2, 4, "Água");
+    private static final Produto agua = new Produto(2, 4, "Água");
 
-    public static final Produto doce = new Produto(1, 5, "Doce");
+    private static final Produto doce = new Produto(1, 5, "Doce");
 
-    public static final Produto salgadinho = new Produto(4, 4, "Salgadinho");
+    private static final Produto salgadinho = new Produto(4, 4, "Salgadinho");
     
-    public static final Produto amendoim = new Produto(2.5, 3, "Amendoim");
+    private static final Produto amendoim = new Produto(2.5, 3, "Amendoim");
 }
